a440790624a0a8f45e37fda26b5dfce599dd39ba
Martin Bohorquez <121200468+martinbohorquez@users.noreply.github.com>
2024-11-05 12:42:59 -0500

    delete calculateTotal and saveSession from cart-app component.

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -17,7 +17,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 export class CartAppComponent implements OnInit {
 	products: Product[] = [];
 	items: CartItem[] = [];
-	total: number = 0;
 	showCart: boolean = false;
 
 	constructor(private productService: ProductService, private cartItemService: CartItemService) {}
@@ -25,35 +24,20 @@ export class CartAppComponent implements OnInit {
 	ngOnInit(): void {
 		this.products = this.productService.findAll();
 		this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
-		this.calculateTotal();
 	}
 
 	onAddCart(product: Product): void {
 		this.items = this.cartItemService.addProduct(this.items, product);
-		this.calculateTotal();
-		this.saveSession();
 	}
 
 	onDeleteCart(id: number): void {
 		this.items = this.cartItemService.removeProduct(this.items, id);
-		this.calculateTotal();
-		this.saveSession();
 		if (this.items.length == 0) {
 			sessionStorage.removeItem('cart');
 		}
 	}
 
-	saveSession(): void {
-		sessionStorage.setItem('cart', JSON.stringify(this.items));
-	}
-
 	openCloseCart(): void {
 		this.showCart = !this.showCart;
-	}
-
-	calculateTotal(): void {
-		this.total = this.items.reduce((accumulator, item) => {
-			return accumulator + item.quantity * item.product.price;
-		}, 0);
 	}
 }
